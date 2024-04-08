7fcb452702936b063f0d377249609f6da4004023
Rômulo Augusto <romuloffall@gmail.com>
2024-04-08 02:25:17 -0300

    #add header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <li>Curvue S <img src="/src/assets/logo.svg" alt="vuejs" /></li>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    display: flex;
+    gap: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    padding: 24px;
+    width: 100%;
+  }
+
+  li {
+    display: block;
+    text-align: center;
+    font-weight: bolder;
+    font-size: 1.5rem;
+  }
+</style>
