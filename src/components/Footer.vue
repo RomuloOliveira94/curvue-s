02e051d070ee8307a56b98c583522def5018830c
Rômulo Augusto <romuloffall@gmail.com>
2024-04-08 02:19:08 -0300

    #add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span>Este projeto desenvolvido por Rômulo Oliveira</span>
+    <nav>
+      <a href="https://www.linkedin.com/in/roliveira94/" target="_blank"
+        >Linkedin</a
+      >
+      <a href="https://github.com/RomuloOliveira94/" target="_blank">GitHub</a>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+  nav {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  span {
+    display: block;
+    text-align: center;
+    margin-top: 1rem;
+  }
+</style>
