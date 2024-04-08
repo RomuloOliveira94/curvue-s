a19af5001cf964d415f9e2f9db18f76db8ba4a6b
Rômulo Augusto <romuloffall@gmail.com>
2024-04-08 01:52:51 -0300

    #add explain section container

---
+++ src/components/ExplainSectionContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section-container">
+    <slot />
+  </section>
+</template>
+
+<style>
+  .section-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1rem;
+  }
+</style>
